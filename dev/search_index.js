b617ef45d93299e1fef3f8e6f3fc6171f74effa9
Documenter.jl <documenter@juliadocs.github.io>
2021-12-01 11:10:11 +0000

    build based on 436ed26

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LockPatternComplexity","category":"page"},{"location":"#LockPatternComplexity","page":"Home","title":"LockPatternComplexity","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LockPatternComplexity.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LockPatternComplexity]","category":"page"}]
+}
