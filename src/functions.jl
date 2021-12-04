6d8d70366a6f232485d848050c68ff86b15939ab
Jaan Tollander de Balsch <jaan@hey.com>
2021-12-04 16:22:32 +0200

    feat: bounds for taxicab distance

--- src/functions.jl
+++ src/functions.jl
@@ -36,14 +36,27 @@ end
 
 function symmetries(n::Int)
     # Horizontal reflection
-    reflect = [j + i*n for i in 0:n-1 for j in n:-1:1]
+    reflect = [j + i * n for i = 0:n-1 for j = n:-1:1]
     # Rotate 90 degrees
-    r90 = [j + i*n for j in 1:n for i in n-1:-1:0]
+    r90 = [j + i * n for j = 1:n for i = n-1:-1:0]
     r180 = r90[r90]
     r270 = r180[r90]
     r180_reflect = r180[reflect]
     r270_reflect = r270[reflect]
     return r90, r180, r270, reflect, r180_reflect, r270_reflect
+end
+
+"""Upper and lower bound for taxicab distance."""
+function bounds(n::Int)
+    M = n^2 - 1
+    ys, xs, ks = line_types(n)
+    # Taxicab distance of each line type
+    x = abs.(ys) + abs.(xs)
+    # Taxicab distance of shortest unique line types
+    d_lb = sum(sort(x)[1:M])
+    # Taxicab distance of longest unique line types
+    d_ub = sum(reverse(sort(x .* ks))[1:M])
+    return d_lb, d_ub
 end
 
 function data(n::Int)
@@ -53,10 +66,12 @@ function data(n::Int)
     xs, ys, ks = line_types(n)
     N = length(ks)
     r90, r180, r270, reflect, r180_reflect, r270_reflect = symmetries(n)
-    return ["n=$n;", "m=$m;", "x=$x;", "y=$y;", "M=$M;", "N=$N;", 
-            "xs=$(xs);", "ys=$(ys);", "ks=$(ks);",
-            "r90=$r90;", "r180=$r180;", "r270=$r270;", "reflect=$reflect;",
-            "r180_reflect=$r180_reflect;", "r270_reflect=$r270_reflect;"]
+    d_lb, d_ub = bounds(n)
+    return ["n=$n;", "m=$m;", "x=$x;", "y=$y;", "M=$M;", "N=$N;",
+        "xs=$(xs);", "ys=$(ys);", "ks=$(ks);",
+        "r90=$r90;", "r180=$r180;", "r270=$r270;", "reflect=$reflect;",
+        "r180_reflect=$r180_reflect;", "r270_reflect=$r270_reflect;", 
+        "d_lb=$d_lb;", "d_ub=$d_ub;"]
 end
 
 function line_type(x1::Int, x2::Int, y1::Int, y2::Int)
@@ -70,7 +85,7 @@ function line_type(x1::Int, x2::Int, y1::Int, y2::Int)
     end
 end
 
-function parse_int_array(x::AbstractString; I=Int)
+function parse_int_array(x::AbstractString; I = Int)
     [parse(I, t.match) for t in eachmatch(r"([0-9]+)", x)]
 end
 
