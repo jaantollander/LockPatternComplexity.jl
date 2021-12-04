7828e04e172f5c2bf559c73542742d4cc06172db
Jaan Tollander de Balsch <jaan@hey.com>
2021-12-04 11:24:25 +0200

    fix: improved plots script

--- scripts/plot.jl
+++ scripts/plot.jl
@@ -1,10 +1,11 @@
 using LockPatternComplexity
 using Plots: savefig
 
-for n in [3, 5]
-    solutions = extract_solutions("results/$(n)x$(n).txt")
+function create_plots(n::Int, path::AbstractString; limit::Int=0)
+    solutions = extract_solutions(path)
     for (distance, patterns) in solutions
-        root = mkpath(joinpath("results", "$(n)x$(n)", "$(distance)"))
+        distance ≤ limit && continue
+        root = mkpath(joinpath("plots", "$(n)x$(n)", "$(distance)"))
         for pattern in patterns
             plt = plot_nxn(n, pattern)
             savefig(plt, joinpath(root, "$(hash(pattern)).svg"))
@@ -12,24 +13,9 @@ for n in [3, 5]
     end
 end
 
-for n in [2, 4, 6]
-    solutions = extract_solutions("results/$(n)x$(n)_gen.txt")
-    for (distance, patterns) in solutions
-        root = mkpath(joinpath("results", "$(n)x$(n)", "$(distance)"))
-        for pattern in patterns
-            plt = plot_nxn(n, pattern)
-            savefig(plt, joinpath(root, "$(hash(pattern)).svg"))
-        end
-    end
-end
-
-for n in [4]
-    solutions = extract_solutions("results/$(n)x$(n)_gen_2.txt")
-    for (distance, patterns) in solutions
-        root = mkpath(joinpath("results", "$(n)x$(n)", "$(distance)"))
-        for pattern in patterns
-            plt = plot_nxn(n, pattern)
-            savefig(plt, joinpath(root, "$(hash(pattern)).svg"))
-        end
-    end
-end
+create_plots(2, "results/2x2_sat.txt"; limit=0)
+create_plots(3, "results/3x3_sat.txt"; limit=20)
+create_plots(4, "results/4x4_opt.txt"; limit=56)
+create_plots(5, "results/5x5_sat.txt"; limit=113)
+create_plots(6, "results/6x6_sat.txt"; limit=193)
+create_plots(7, "results/7x7_sat.txt"; limit=0)
